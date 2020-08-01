3bb08da15529ad7b2f4eddd2bee1635baa43e2b6
Jessica <duellje@oregonstate.edu>
2020-08-01 15:13:39 -0400

    setting up api

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// implement your API here
+const express = require('express');
+const port = 8000;
+
+const server = express();
+
+//endpoint
+server.get('/', (req, res)=>{
+    res.send('Hello World');
+});
+
+server.listen(port, () => {
+    console.log(`API running on port ${port}`);
+});
