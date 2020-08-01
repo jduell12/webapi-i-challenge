9f84f723e68af2561065b61785c444c89ef7d336
Jessica <duellje@oregonstate.edu>
2020-08-01 15:28:29 -0400

    endpoing that returns list of hobbits

--- index.js
+++ index.js
@@ -5,8 +5,25 @@ const server = express();
 
 //endpoint
 server.get('/', (req, res)=>{
-    res.send('Hello World');
+    res.send('Hello Worl!');
 });
+
+//endpoint returns list of hobbits
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1, 
+            name: 'Samwise Gamgee'
+        }, 
+        {
+            id: 2,
+            name: 'Frodo Baggins'
+        }
+    ];
+
+    //200 - successful
+    res.status(200).json(hobbits);
+})
 
 server.listen(port, () => {
     console.log(`API running on port ${port}`);
